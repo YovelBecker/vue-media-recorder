fb8f9d2cd3fbc76a695d0b3f835c1c11a0f8c80c
Yovel Becker <yovelb13@gmail.com>
2019-07-10 12:45:32 +0300

    after first successful build

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+// module.exports = {
+//     publicPath: ‘<my-first-project>’
+//     } 
+
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/img-vid-proj/'
+        : '/'
+}
